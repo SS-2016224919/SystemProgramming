834fb4806ffffc29f46c08f0723d0c7856a87391
hcpark <hcpark@knu.ac.kr>
2016-04-07 20:26:20 +0900

    exercise3

---
+++ exercise3/2016224919_PE3.c
@@ -0,0 +1,98 @@
+#include	<stdio.h>
+#include	<sys/types.h>
+#include	<sys/stat.h>
+#include	<dirent.h>
+
+ino_t	get_inode(char *);
+void    printpathto_recursive(ino_t);
+void    inum_to_name(ino_t , char *, int );
+void	printpathto_loop(ino_t);
+
+int main()
+{
+	printpathto_loop(get_inode("."));	/* print path to here	*/
+	putchar('\n');				/* then add newline	*/
+	return 0;
+}
+
+void printpathto_recursive( ino_t this_inode )
+/*
+ *	prints path leading down to an object with this inode
+ *	kindof recursive
+ */
+{
+	ino_t	my_inode ;
+	char	its_name[BUFSIZ];
+
+	if ( get_inode("..") != this_inode )
+	{
+		chdir( ".." );				/* up one dir	*/
+
+		inum_to_name(this_inode,its_name,BUFSIZ);/* get its name*/
+
+		my_inode = get_inode( "." );		/* print head	*/
+		printpathto_recursive( my_inode );		/* recursively	*/
+		printf("/%s", its_name );		/* now print	*/
+							/* name of this	*/
+	}
+}
+
+void printpathto_loop(ino_t this_inode){
+        char its_name[100][BUFSIZ];
+	int dirCount=0;
+        while(get_inode("..") != this_inode){
+		chdir("..");
+		inum_to_name( this_inode, its_name[dirCount], BUFSIZ);
+		this_inode = get_inode(".");
+		dirCount++;
+	}
+	for(;0<dirCount;dirCount--){
+		printf("/%s",its_name[dirCount-1]);
+	}
+}
+
+void inum_to_name(ino_t inode_to_find , char *namebuf, int buflen)
+/*
+ *	looks through current directory for a file with this inode
+ *	number and copies its name into namebuf
+ */
+{
+	DIR		*dir_ptr;		/* the directory */
+	struct dirent	*direntp;		/* each entry	 */
+
+	dir_ptr = opendir( "." );
+	if ( dir_ptr == NULL ){
+		perror( "." );
+		exit(1);
+	}
+
+	/*
+	 * search directory for a file with specified inum
+	 */
+
+	while ( ( direntp = readdir( dir_ptr ) ) != NULL )
+		if ( direntp->d_ino == inode_to_find )
+		{
+			strncpy( namebuf, direntp->d_name, buflen);
+			namebuf[buflen-1] = '\0';   /* just in case */
+			closedir( dir_ptr );
+			return;
+		}
+	fprintf(stderr, "error looking for inum %d\n", inode_to_find);
+	exit(1);
+}
+
+ino_t get_inode( char *fname )
+/*
+ *	returns inode number of the file
+ */
+{
+	struct stat info;
+
+	if ( stat( fname , &info ) == -1 ){
+		fprintf(stderr, "Cannot stat ");
+		perror(fname);
+		exit(1);
+	}
+	return info.st_ino;
+}
